61f626a47bef60408abb41bb964249c32e10f29b
purnendra19 <92633709+purnendra19@users.noreply.github.com>
2022-09-27 22:34:51 +0530

    Add files via upload

---
+++ Calculation program in java/Advancecalculater.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Advancecalculater{
+public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+        System.out.printf(" Select 1 For Addition : %n Select 2 For Subtraction : %n Select 3 For Multiplication : %n Select 4 For Division :");
+        int botton = sc.nextInt();
+        float d;
+        int r;
+        switch(botton){
+           case 1 : d=a+b;
+           System.out.print(+d);
+           break;
+           
+           case 2: d=a-b;
+           System.out.print(+d);
+           break;
+
+           case 3: d=a*b;
+           System.out.print(+d);
+           break;
+
+           case 4: d=a/b ; r = a%b;
+           System.out.println("bagfal"+d);
+           System.out.println("remender"+r);
+           break;
+
+           default : System.out.print("invalid botton");
+
+           
+        }sc.close();
+
+      
+
+    }
+}
